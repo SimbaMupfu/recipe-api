a05e26f340653d4b7161d639b7c2016924143fa1
simba_m <scmupfururirwa@gmail.com>
2021-09-04 15:04:50 +0200

    implement feature for filtering tags and ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -95,7 +95,7 @@ class PrivateIngredientsApiTests(TestCase):
         )
         recipe.ingredients.add(ingredient1)
 
-        res = self.client.get(INGREDIENTS_URL, {'assigned-only': 1})
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
 
         serializer1 = IngredientSerializer(ingredient1)
         serializer2 = IngredientSerializer(ingredient2)
@@ -123,6 +123,6 @@ class PrivateIngredientsApiTests(TestCase):
         )
         recipe2.ingredients.add(ingredient)
 
-        res = self.client.get(INGREDIENTS_URL, {'assigned-only': 1})
+        res = self.client.get(INGREDIENTS_URL, {'assigned_only': 1})
 
         self.assertEqual(len(res.data), 1)
